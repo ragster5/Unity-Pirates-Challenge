0eaa5d6756046d0480eff750ad9f7bb72efb59e9
ragster5 <alex_xandre93@hotmail.com>
2023-02-12 16:02:55 -0300

    IA e Arte
    - Fazendo ajustes na IA do Chaser
    - Adicionando os assets no projeto
    - Implemento a arte do player

--- PiratesChallenge/Assets/Scripts/Chaser.cs
+++ PiratesChallenge/Assets/Scripts/Chaser.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Chaser : Enemy
 {
-    public Transform player;
-    Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
         currentLife = lifeMax;
-        body = GetComponent<Rigidbody2D>();
         currentLife = lifeBar.UpdateBar(0, currentLife, lifeMax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.Lerp(transform.position, player.position, 0.2f * Time.deltaTime);
         lifeBar.transform.position = transform.position;
+        switch (iaState)
+        {
+            case IAStates.Chase:
+                Chase();
+                break;
+            case IAStates.Obstacle:
+                break;
+            case IAStates.Shoot:
+                break;
+            default:
+                break;
+        }
     }
 }
--- PiratesChallenge/Assets/Scripts/Enemy.cs
+++ PiratesChallenge/Assets/Scripts/Enemy.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum IAStates
+{
+    Chase, Obstacle, Shoot, Rotate
+}
 public abstract class Enemy : MonoBehaviour
 {
     public float lifeMax, speed;
     protected float currentLife;
     public LifeBar lifeBar;
+    public Transform player;
+    protected IAStates iaState = IAStates.Chase;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,4 +21,9 @@ public abstract class Enemy : MonoBehaviour
             Destroy(transform.parent.gameObject);
         }
     }
+    protected void Chase()
+    {
+        transform.position = Vector2.Lerp(transform.position, player.position, speed * Time.deltaTime);
+    }
+    
 }
