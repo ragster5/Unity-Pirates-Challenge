b8b96775c41f1a3c9c9bdf2f84a2d023ed3dd019
ragster5 <alex_xandre93@hotmail.com>
2023-02-14 15:47:54 -0300

    Menu Título
    -Tela de título: Toda a parte mecânica implementada

--- PiratesChallenge/Assets/Scripts/Title.cs
+++ PiratesChallenge/Assets/Scripts/Title.cs
@@ -2,12 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class Title : MonoBehaviour
 {
+    public TextMeshProUGUI matchDurationTxt, spawnTimeTxt;
+    int matchDuration = 2, spawnTime = 5;
    public void PlayBtn()
     {
         SceneManager.LoadScene("Game");
     }
     
+    public void ModifyMatchDuration(int value)
+    {
+        if(value < 0 && matchDuration == 1 || value > 0 && matchDuration == 3)
+        {
+            return;
+        }
+        matchDuration += value;
+        PlayerPrefs.SetInt("MatchDuration", matchDuration);
+        matchDurationTxt.text = matchDuration.ToString();
+    }
+    public void ModifySpawnDuration(int value)
+    {
+        if(value <0 && spawnTime == 1)
+        {
+            return;
+        }
+        spawnTime += value;
+        PlayerPrefs.SetInt("SpawnTime", spawnTime);
+        spawnTimeTxt.text = spawnTime.ToString();
+    }
+
 }
