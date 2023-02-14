6ce69c7799b98b508e7d9b7ea409b9207c443ab1
ragster5 <alex_xandre93@hotmail.com>
2023-02-14 18:19:30 -0300

    Sprite Alterar conforme danos
    -Implementada função do barco do player alterar a sprite de acordo com a vida
    -Sistema ainda precisa ser otimizado para funcionar nos inimigos também

--- PiratesChallenge/Assets/Scripts/Player.cs
+++ PiratesChallenge/Assets/Scripts/Player.cs
@@ -8,17 +8,18 @@ public class Player : MonoBehaviour
     public GameObject explosion;
     public Transform frontalGun, lifeBarPos;
     public Transform[] sideGun = new Transform[6];
+    public SpriteRenderer spriteShip;
     [Header("Modifiers")]
     public float moveSpeed = 5f;
     public float rotationSpeed = 5f, lifeMax = 10f;
-    Rigidbody2D body;
     float rotate = 0, currentLife;
     LifeBar lifeBar;
     [Header("Sprites")]
     public Sprite[] shipCondition;
     public Sprite[] flagCondition;
 
-    //]
+    //Referencias privadas
+    Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
@@ -67,11 +68,24 @@ public class Player : MonoBehaviour
     }
     void SpriteController()
     {
-
+        //Troca da sprite do barco
+        int state = (int)((currentLife * 10) / lifeMax);
+        if(state > 7)
+        {
+            spriteShip.sprite = shipCondition[2];
+        } else if (state > 4)
+        {
+            spriteShip.sprite = shipCondition[1];
+        } else if (state > 2)
+        {
+            spriteShip.sprite = shipCondition[0];
+        } 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        currentLife = lifeBar.UpdateBar(collision.GetComponent<Bullet>().damage,currentLife, lifeMax);
+        //currentLife = lifeBar.UpdateBar(collision.GetComponent<Bullet>().damage,currentLife, lifeMax);
+        currentLife = lifeBar.UpdateBar(2,currentLife, lifeMax);
         Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+        SpriteController();
     }
 }
