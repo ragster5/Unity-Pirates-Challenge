957f701b00d8b36a6ab4c74dd80e2204295aaf72
ragster5 <alex_xandre93@hotmail.com>
2023-02-19 16:50:03 -0300

    IA e Cenário
    -Importado NavMesh2D
    -Começo da reestruturação da IA dos inimigos
    -Criado as regras de tilemap
    -Começando a montar o mapa

--- PiratesChallenge/Assets/Scripts/Enemy.cs
+++ PiratesChallenge/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 public enum IAStates
 {
     Deciding, Moving, Arrive
@@ -15,6 +16,7 @@ public abstract class Enemy : MonoBehaviour
     public LifeBar lifeBar;
     public Transform player;
     public GameController gc;
+    protected NavMeshAgent agent;
     protected IAStates iaState = IAStates.Deciding;
 
 
@@ -30,40 +32,6 @@ public abstract class Enemy : MonoBehaviour
             gc.UpdateScore(points);
             Destroy(transform.parent.gameObject);
         }
-    }
-    
-    protected void Chase()
-    {
-        transform.position = Vector2.Lerp(transform.position, player.position, speed/10 * Time.deltaTime);
-    }
-    protected void Vision()
-    {
-        RaycastHit2D ray = Physics2D.Linecast(transform.position, player.position, 7);
-        if (!ray.collider.CompareTag("Player"))
-        {
-           // iaState = IAStates.Obstacle;
-        } else
-        {
-            //iaState = IAStates.Chase;
-        }
-    }
-    protected void Obstacle()
-    {
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.up, 3, 7);
-        if(!ray)
-        {
-            target = new Vector2(Random.Range(0, 2), Random.Range(0, 3));
-            iaState = IAStates.Moving;
-        }
-    }
-    protected void Moving()
-    {
-        transform.position = Vector2.Lerp(transform.position, target, speed / 10 * Time.deltaTime);
-        if (transform.position.Equals(target))
-        {
-           //    iaState = IAStates.Obstacle;
-        }
-        Vision();
     }
     
 }
--- PiratesChallenge/Assets/Scripts/Shooter.cs
+++ PiratesChallenge/Assets/Scripts/Shooter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Shooter : Enemy
 {
@@ -9,18 +10,17 @@ public class Shooter : Enemy
     public Transform frontal;
 
     [Header("Modifiers")]
-    public float timeBetweenShots = 1;
-    public float distanceToShoot = 3;
 
 
     float nextShotTime;
     bool chasing;
-    Rigidbody2D body;
     float angle;
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
         currentLife = lifeMax;
         currentLife = lifeBar.UpdateBar(0, currentLife, lifeMax);
         iaState = IAStates.Deciding;
@@ -29,81 +29,22 @@ public class Shooter : Enemy
     // Update is called once per frame
     void Update()
     {
-        print(iaState);
         lifeBar.transform.position = transform.position;
         switch (iaState)
         {
             case IAStates.Deciding:
-                RaycastHit2D ray = Physics2D.Linecast(transform.position, player.position, 7);
-                if (ray.collider.CompareTag("Player"))
-                {
-                    chasing = true;
-                    if(Vector2.Distance(transform.position, target) < distanceToShoot)
-                    {
-                        iaState = IAStates.Arrive;
-                    } else
-                    {
-                        target = player.position;
-                        iaState = IAStates.Moving;
-                    }
-                } else
-                {
-                    //RandomLocation(3);
-                    RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.parent.up, 4, 7);
-                    if (hitInfo.collider != null)
-                    {
-                        Debug.DrawRay(transform.position, hitInfo.point, Color.red);
-                    }
-                    else
-                    {
-                        Debug.DrawRay(transform.position, transform.position + transform.up * 4, Color.blue);
-                    }
-                    /*
-                    if (Vector3.Distance(target, transform.parent.position) > 1)
-                    {
-                        iaState = IAStates.Moving;
-                    }
-                    */
-                }
+                
                 break;
             case IAStates.Moving:
-                if (Vector2.Distance(transform.position, target) > distanceToShoot)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-                } else
-                {
-                    iaState = IAStates.Arrive;
-                }
+                
                 break;
             case IAStates.Arrive:
-                if (chasing)
-                {
-                    if(Time.time > nextShotTime)
-                    {
-                        Instantiate(bullet, frontal.position, frontal.rotation);
-                        nextShotTime = Time.time + timeBetweenShots;
-                    }
-                }
+                
                 break;
         }
     }
     void Shoot()
     {
 
-    }
-    public void RandomLocation(float radius)
-    {
-        Vector3 randomDirection = Random.insideUnitCircle * radius;
-        randomDirection += transform.position;
-        RaycastHit2D ray = Physics2D.Linecast(transform.position, randomDirection, 7);
-        Debug.DrawRay(transform.position, randomDirection, Color.black);
-        if (!ray.collider)
-        {
-            target = ray.point;
-        } else
-        {
-            target = transform.position;
-        }
-        print(target);
     }
 }
